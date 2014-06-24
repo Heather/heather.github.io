385f98feae89cf7139f7ff23bdf2f0f96921c876
Heather <heather@live.ru>
2014-06-24 10:11:02 +0400

    modified:   .gitignore 	new file:   darcs-build.cmd 	modified:   index.html 	modified:   pubspec.yaml 	modified:   src/templates/index.html 	modified:   update.cmd 	modified:   update.sh 	new file:   web/hi.dart 	new file:   web/hi.dart.js 	new file:   web/hi.dart.precompiled.js 	new file:   web/packages/browser/dart.js 	new file:   web/packages/browser/interop.js

--- update.cmd
+++ update.cmd
@@ -1,6 +1,7 @@
 @echo off
 
 ::Clean
+rm -rf build
 rm -rf clay
 rm -rf posts
 rm -rf index.html
@@ -14,6 +15,9 @@ chcp 65001
 
 ghc --make src/hs/site.hs -o src/site.exe
 
+::Dart
+cp -rf build/web web/
+
 ::Build
 pushd .
 cd .\src
@@ -22,7 +26,8 @@ set ABS_PATH=%CD%
 "%CD%/site.exe" clean
 "%CD%/site.exe" build
 
-cp -rf %CD%/_site/* %CD%/..
+::Hakyll
+cp -rf %CD%/_site/* %CD%/../
 
 rm -rf %CD%/_cache
 rm -rf %CD%/_site
--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 #clean
+rm -rf build
 rm -rf clay
 rm -rf posts
 rm -rf index.html
@@ -10,12 +11,16 @@ export LANG=en_US.UTF-8
 #buld
 ghc --make src/hs/site.hs -o src/site
 
+#Dart
+cp -rf build/web web/
+
 cd src
 
 ./site clean
 ./site build
 
-cp -rf _site/* ..
+#Hakyll
+cp -rf _site/* ../
 
 rm -rf _cache
 rm -rf _site
