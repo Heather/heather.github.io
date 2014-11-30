8daa1d99d6ef4baea0acf22cc7dc3641994b640a
Heather <heather@live.ru>
2014-11-30 20:51:33 +0400

    minified eye.js

--- src/js/eyes.js
+++ src/js/eyes.js
@@ -1,4 +1,3 @@
-
 /**
  * Copyright (C) 2011 Hakim El Hattab, http://hakim.se
  * 
@@ -20,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  * 
- * @author Hakim El Hattab | http://hakim.se
+ * @author Hakim El Hattab
  */
 (function(){
 	
@@ -87,13 +86,10 @@
 	}
 	
 	function animate() {
-		// The number of seconds that have passed since initialization
 		var seconds = ( Date.now() - startTime ) / 1000;
 		
-		// Out with the old ...
 		context.clearRect( 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT );
 		
-		// ... in with the new
 		for( var i = 0, len = eyes.length; i < len; i++ ) {
 			var eye = eyes[i];
 			
@@ -103,47 +99,38 @@
 			eye.update( mouse );
 		}
 		
-		// Remove the overlay if its time has passed
 		if( seconds > OVERLAY_DURATION && overlay !== undefined ) {
 			
-			// Ease-in
 			overlayOpacity *= 0.94 + ( 0.055 * overlayOpacity );
 			overlayOpacity = Math.max( overlayOpacity - 0.01, 0 );
 			
 			overlay.style.opacity = overlayOpacity;
 			
 			if( overlayOpacity === 0 ) {
-				// We have no more use for the overlay, removing it ensures
-				// that we do not repeatedly enter this if statement
 				container.removeChild( overlay );
-				
 				overlay = undefined;
 			}
 		}
-		
 		requestAnimFrame( animate );
 	}
-	
 	window.addEventListener ? window.addEventListener( 'load', initialize, false ) : window.onload = initialize;
-	
 })();
-
 
 function Eye( canvas, x, y, scale, time ) {
 	this.canvas = canvas;
-	this.context = this.canvas.getContext( '2d' )
+	this.context = this.canvas.getContext( '2d' );
 	
-	// The time at which this eye will come alive
+	/* The time at which this eye will come alive */
 	this.activationTime = time;
 	
-	// The speed at which the iris follows the mouse
+	/* The speed at which the iris follows the mouse */
 	this.irisSpeed = 0.01 + ( Math.random() * 0.2 ) / scale;
 	
-	// The speed at which the eye opens and closes
+	/* The speed at which the eye opens and closes */
 	this.blinkSpeed = 0.2 + ( Math.random() * 0.2 );
 	this.blinkInterval = 5000 + 5000 * ( Math.random() );
 	
-	// Timestamp of the last blink
+	/* Timestamp of the last blink */
 	this.blinkTime = Date.now();
 	
 	this.scale = scale;
@@ -170,20 +157,20 @@ function Eye( canvas, x, y, scale, time ) {
 		target: 1
 	};
 	
-	// Affects the amount of inner shadow
+	/* Affects the amount of inner shadow */
 	this.tiredness = ( 0.5 - this.exposure.top ) + 0.1;
 	
 	this.isActive = false;
 	
 	this.activate = function() {
 		this.isActive = true;
-	}
+	};
 	
 	this.update = function( mouse ) {
 		if( this.isActive === true ) {
 			this.render( mouse );
 		}
-	}
+	};
 	
 	this.render = function( mouse ) {
 		var time = Date.now();
@@ -198,34 +185,34 @@ function Eye( canvas, x, y, scale, time ) {
 		
 		this.exposure.current += ( this.exposure.target - this.exposure.current ) * this.blinkSpeed;
 		
-		// Eye left/right
+		/* Eye left/right */
 		var el = { x: this.x - ( this.size * 0.8 ), y: this.y - ( this.size * 0.1 ) };
 		var er = { x: this.x + ( this.size * 0.8 ), y: this.y - ( this.size * 0.1 ) };
 		
-		// Eye top/bottom
+		/* Eye top/bottom */
 		var et = { x: this.x, y: this.y - ( this.size * ( 0.5 + ( this.exposure.top * this.exposure.current ) ) ) };
 		var eb = { x: this.x, y: this.y - ( this.size * ( 0.5 - ( this.exposure.bottom * this.exposure.current ) ) ) };
 		
-		// Eye inner shadow top
+		/* Eye inner shadow top */
 		var eit = { x: this.x, y: this.y - ( this.size * ( 0.5 + ( ( 0.5 - this.tiredness ) * this.exposure.current ) ) ) };
 		
-		// Eye iris
+		/* Eye iris */
 		var ei = { x: this.x, y: this.y - ( this.iris.size ) };
 		
-		// Offset the iris depending on mouse position
+		/* Offset the iris depending on mouse position */
 		var eio = { 
 			x: ( mouse.x - ei.x ) / ( window.innerWidth - ei.x ), 
 			y: ( mouse.y ) / ( window.innerHeight )
 		};
 		
-		// Apply the iris offset
+		/* Apply the iris offset */
 		ei.x += eio.x * 16 * Math.max( 1, this.scale * 0.4 );
 		ei.y += eio.y * 10 * Math.max( 1, this.scale * 0.4 );
 		
 		this.iris.x += ( ei.x - this.iris.x ) * this.irisSpeed;
 		this.iris.y += ( ei.y - this.iris.y ) * this.irisSpeed;
 		
-		// Eye fill drawing
+		/* Eye fill drawing */
 		this.context.fillStyle = 'rgba(190,190,190,5.0)';
 		this.context.strokeStyle = 'rgba(50,50,50,1.0)';
 		this.context.beginPath();
@@ -238,7 +225,7 @@ function Eye( canvas, x, y, scale, time ) {
 		this.context.stroke();
 		this.context.fill();
 		
-		// Iris
+		/* Iris */
 		this.context.save();
 		this.context.globalCompositeOperation = 'source-atop';
 		this.context.translate(this.iris.x*0.1,0);
@@ -252,7 +239,7 @@ function Eye( canvas, x, y, scale, time ) {
 		this.context.stroke();
 		this.context.restore();
 		
-		// Iris inner
+		/* Iris inner */
 		this.context.save();
 		this.context.shadowColor = 'rgba(255,255,255,0.5)';
 		this.context.shadowOffsetX = 0;
@@ -267,7 +254,7 @@ function Eye( canvas, x, y, scale, time ) {
 		this.context.fill();
 		this.context.restore();
 		
-		// Pupil
+		/* Pupil */
 		this.context.save();
 		this.context.globalCompositeOperation = 'source-atop';
 		this.context.fillStyle = 'rgba(0,0,0,0.9)';
@@ -284,7 +271,7 @@ function Eye( canvas, x, y, scale, time ) {
 		this.context.shadowOffsetY = 0;
 		this.context.shadowBlur = 10;
 		
-		// Eye top inner shadow
+		/* Eye top inner shadow */
 		this.context.fillStyle = 'rgba(120,120,120,0.2)';
 		this.context.beginPath();
 		this.context.moveTo( el.x, el.y );
@@ -298,7 +285,6 @@ function Eye( canvas, x, y, scale, time ) {
 	}
 }
 
-// shim with setTimeout fallback from http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame       || 
           window.webkitRequestAnimationFrame || 
