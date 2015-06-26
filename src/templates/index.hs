002677c5039c0961b26dd8524fc5324a8cdfbd17
Heather <heather@live.ru>
2015-06-26 15:26:41 +0400

    improve Index Lucid template readability

--- src/templates/index.hs
+++ src/templates/index.hs
@@ -15,16 +15,16 @@ import qualified Data.Text.Lazy as L
 page :: Html ()
 page = do
   link_ [rel_ "stylesheet", type_ "text/css", href_ "css/index.css"]
-  div_ [id_ "fof"]
-   $ do div_ $ img_ [style_ "position: absolute;", src_ "/images/eyes.png"]
-        canvas_ [id_ "canvas"] ""
-  div_ [id_ "parent"]
-    $ div_ [id_ "top"]
-      $ div_ [class_ "mlr"]
-       $ do div_ [class_ "mleft"]
-              $ ul_ [class_ "recent-posts"]
-                $ raw "$partial(\"templates/post-list.html\")$"
-            div_ [class_ "mright"] $ img_ [id_ "img", src_ "/images/Hlogo.png"]
+  div_ [id_ "fof"] $ do
+    div_ $ img_ [style_ "position: absolute;", src_ "/images/eyes.png"]
+    canvas_ [id_ "canvas"] ""
+  div_ [id_ "parent"] $
+    div_ [id_ "top"] $
+      div_ [class_ "mlr"] $ do
+        div_ [class_ "mleft"] $
+          ul_ [class_ "recent-posts"] $
+            raw "$partial(\"templates/post-list.html\")$"
+        div_ [class_ "mright"] $ img_ [id_ "img", src_ "/images/Hlogo.png"]
   div_ [id_ "main"] "" --fullscreen?
   script "js/eyes.js"
   script "js/youtube.js"
