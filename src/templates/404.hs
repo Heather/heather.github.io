2d0631cf1143585d274282394c27f48a8a77894f
Heather <heather@live.ru>
2015-06-27 15:24:03 +0400

    Apply Lucid to default hakyll template

--- src/templates/404.hs
+++ src/templates/404.hs
@@ -18,32 +18,32 @@ page = do
   link_ [rel_ "stylesheet", type_ "text/css", href_ "./css/index.css"]
   link_ [rel_ "stylesheet", type_ "text/css", href_ "./css/404.css"]
   div_ [id_ "main"] "" --fullscreen?
-  div_ [id_ "parent"]
-    $ div_ [id_ "top"]
-      $ div_ [class_ "mlr"]
-       $ do div_ [class_ "mleft", style_ "margin-top: 50px;"]
-              "404"
-            div_ [class_ "mright", style_ "padding-top: 250px;"]
-             $ do h3_ "Hello {{name}}!"
-                  "Name:"
-                  input_ [type_ "text", ng_model_ "name"]
-                  br_ []
-                  button_ [class_ "btn btn-primary", data_target_ "#modal", data_toggle_ "modal"] "Ping"
-                  div_ [id_ "modal", class_ "modal fade", tabindex_ "-1", role_ "dialog"]
-                   $ div_ [class_ "modal-dialog"]
-                    $ div_ [class_ "modal-content"]
-                     $ do div_ [class_ "modal-header"]
-                            $ do button_ [type_ "button", class_ "close", data_dismiss_ "modal"]
-                                  $ raw "&times"
-                                 h3_ [style_ "margin: 0"] "Pong"
-                          div_ [class_ "modal-body"]
-                           $ ul_ [style_ "margin: 0"]
-                                $ do li_ "Hakyll"
-                                     li_ "Lucid"
-                                     li_ "Clay"
-                          div_ [class_ "modal-footer", style_ "margin: 0"]
-                           $ button_ [class_ "btn btn-success", data_dismiss_ "modal"] "OK"
-  script "./js/light.js"
+  div_ [id_ "parent"] $
+    div_ [id_ "top"] $
+      div_ [class_ "mlr"] $ do
+        div_ [class_ "mleft", style_ "margin-top: 50px;"] "404"
+        div_ [class_ "mright", style_ "padding-top: 250px;"] $ do
+          h3_ "Hello {{name}}!"
+          "Name:"
+          input_ [type_ "text", ng_model_ "name"]
+          br_ []
+          button_ [class_ "btn btn-primary", data_target_ "#modal", data_toggle_ "modal"] "Ping"
+          div_ [id_ "modal", class_ "modal fade", tabindex_ "-1", role_ "dialog"] $
+            div_ [class_ "modal-dialog"] $
+              div_ [class_ "modal-content"] $ do
+                div_ [class_ "modal-header"] $ do
+                  button_ [type_ "button", class_ "close", data_dismiss_ "modal"] $
+                    raw "&times"
+                  h3_ [style_ "margin: 0"] "Pong"
+                  div_ [class_ "modal-body"] $
+                    ul_ [style_ "margin: 0"] $ do
+                      li_ "Hakyll"
+                      li_ "Lucid"
+                      li_ "Clay"
+                div_ [class_ "modal-footer", style_ "margin: 0"] $
+                  button_ [class_ "btn btn-success", data_dismiss_ "modal"] "OK"
+  -- disable awesome js animation in flavor of youtube
+  -- script "./js/light.js"
   script "./js/404.js"
   script "./js/youtube404.js"
  where
---
+++ src/templates/default.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE
+    OverloadedStrings
+  , UnicodeSyntax
+  #-}
+
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module Main where
+
+import           Lucid
+import           Lucid.Base (makeAttribute)
+
+import           Data.Text (Text)
+import qualified Data.Text.Lazy as L
+
+page :: Html ()
+page = do
+  raw "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+  raw "<!DOCTYPE html>"
+  html_ [ngapp_] $ do
+    head_ $ do
+      meta_ [charset_ "utf-8"]
+      meta_ [httpEquiv_ "X-UA-Compatible", content_ "IE=edge,chrome=1"]
+      meta_ [name_ "description", content_ "Heather"]
+      meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1.0"]
+
+      link_ [href_ "/atom.xml", rel_ "alternate", title_ "Heather", type_ "application/atom+xml"]
+      link_ [rel_ "shortcut icon", type_ "image/ico", href_ "/favicon.ico"]
+
+      script "/bootstrap/jquery-2.1.1.min.js"
+      link_ [rel_ "stylesheet", href_ "/bootstrap/bootstrap.min.css"]
+      link_ [rel_ "stylesheet", href_ "/bootstrap/bootstrap-theme.min.css"]
+      script "/bootstrap/bootstrap.min.js"
+      script "/bootstrap/angular.min.js"
+
+      link_ [rel_ "stylesheet", type_ "text/css", media_ "all", href_ "/css/hasklig.css" ]
+      link_ [rel_ "stylesheet", type_ "text/css", media_ "all", href_ "/css/octicons.css"]
+      link_ [rel_ "stylesheet", type_ "text/css", media_ "all", href_ "/css/syntax.css"  ]
+      link_ [rel_ "stylesheet", type_ "text/css", media_ "all", href_ "/css/default.css" ]
+
+      raw "<title>Heather - $title$</title>"
+
+      script "/js/auto.js"
+
+    body_ $ do
+      div_ [class_ "band"] ""
+      div_ [id_ "header"] $
+        div_ [id_ "logo"] $ do
+          a_ [id_ "abbr", href_ "/"] ""
+          script_ "e = document.getElementById(\"abbr\");setTimeout(r, 0);"
+      div_ [id_ "content"] $ raw "$body$"
+      div_ [id_ "social"] $
+        ul_ $ do
+          li_ $
+            a_ [href_ "http://twitter.com/Cynede", title_ "Twitter", target_ "_blank"] $
+              span_ [class_ "mega-octicon octicon-star"] ""
+          li_ $
+            a_ [href_ "mailto:heather@live.ru", title_ "mail", target_ "_blank"] $
+              span_ [class_ "mega-octicon octicon-mail-read"] ""
+          li_ $
+            a_ [href_ "http://www.last.fm/user/Cynede", title_ "Last.fm", target_ "_blank"] $
+              span_ [class_ "mega-octicon octicon-broadcast"] ""
+          li_ $
+            a_ [href_ "http://stackoverflow.com/users/238232/heather", title_ "Stackoverflow", target_ "_blank"] $
+              span_ [class_ "mega-octicon octicon-squirrel"] ""
+          li_ $
+            a_ [href_ "/atom.xml", title_ "RSS", target_ "_blank"] $
+              span_ [class_ "mega-octicon octicon-rss"] ""
+      div_ [id_ "footer"] ""
+ where
+  -- | The @ng-app@ attribute.
+  ngapp_ :: Attribute
+  ngapp_ = makeAttribute "ng-app" ""
+
+  raw :: Monad m => Text → HtmlT m ()
+  raw = toHtmlRaw
+
+  script :: Monad m => Text → HtmlT m ()
+  script s = script_ [src_ s] empt
+   where empt :: Text
+         empt = ""
+
+main :: IO ()
+main = putStr $ L.unpack (renderText page)
