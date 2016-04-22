f9cb7d67486840496f66d7ca487524f34049b160
Heather <heather@live.ru>
2016-04-22 13:25:38 +0400

    silly shake file

--- shake.it.hs
+++ shake.it.hs
@@ -1,6 +1,9 @@
-{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE
+    UnicodeSyntax
+  , CPP
+  #-}
 
--- work in progress
+-- TODO: Linux support
 
 import Shake.It.Off
 
@@ -13,14 +16,51 @@ import Control.Monad
 main :: IO ()
 main = shake $ do
   "clean" ∫ do
-    putStrLn "Cleaning..."
-    removeFile index
-    removeFile "404.html"
-    removeDirectoryRecursive "css"
-    removeDirectoryRecursive "posts"
+    putStrLn " -> Cleaning..."
+    removeIfExists index
+    removeIfExists "404.html"
+    removeIfExists "css"
+    removeIfExists "posts"
+    removeDirIfExists "js"
+    cwd ← getCurrentDirectory
+    let srcDir = cwd </> "src"
+        tempCache = srcDir </> "_cache"
+        tempSite = srcDir </> "_site"
+    removeDirIfExists tempCache
+    removeDirIfExists tempSite
 
   index ◉ ["clean"] ♯♯ do
+    putStrLn " -> Building..."
     ghc ["--make", siteSrc, "-o", siteOut]
+    cwd ← getCurrentDirectory
+    putStrLn " -> Change current dir to src"
+    let srcDir = cwd </> "src"
+        siteHK = srcDir </> "site.exe"
+        tempSite = srcDir </> "_site"
+        tempCache = srcDir </> "_cache"
+        site a =
+          system ("chcp 65001 && " ++ siteHK ++ " " ++ a)
+            >>= checkExitCode
+    srcExists ← doesDirectoryExist srcDir
+    when srcExists $ do
+      setCurrentDirectory srcDir
+      siteHKExists ← doesFileExist siteHK
+      when siteHKExists $ do
+        putStrLn " -> Running hakyll"
+        site "clean"
+        site "build"
+        putStrLn " -> Copy _site back to rootDir"
+        _tmpContent ← getDirectoryContents tempSite
+        let _tmp = filter (`notElem` [".", "..", "src"]) _tmpContent
+        forM_ _tmp $ \name →
+          let srcPath = tempSite </> name
+              dstPath = ".." </> name
+          in doesDirectoryExist srcPath >>= \dirExist →
+              if dirExist then copyDir srcPath dstPath
+                          else copyFile srcPath dstPath
+        putStrLn " -> Removing temp folders"
+        removeDirIfExists tempCache
+        removeDirIfExists tempSite
 
  where index :: String
        index = "index.html"
